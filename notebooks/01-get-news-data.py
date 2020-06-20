04b2dc0d5a49dbd90715bf0f289ff3a7dde0b538
Hikmatyar Hasan <hash@tessella.co.uk>
2020-06-20 19:52:58 +0100

    using get-old-tweets-3 library

--- notebooks/01-get-news-data.py
+++ notebooks/01-get-news-data.py
@@ -13,26 +13,140 @@
 #     name: python3
 # ---
 
-# +
-import OpenBlender, os
-import pandas as pd
-import json
-import tweepy
-
 from HelperFunctions import (
-    SetUpEnvironmentVariables,
-    GetTweetsDataFrame
+    SaveLoadOfOldTweetsIntoCSV
 )
+from datetime import date
+
+
+SaveLoadOfOldTweetsIntoCSV(
+    'USATODAY', 
+    date(2020,6,2), 
+    date(2020,6,13),
+    'test-tweets.csv'
+)
+
+
+
+
+# +
+
+def CreateDatesList(fromDate, toDate):
+    """
+    Create a vector of dates spaced by a week between the two input dates
+    """
+    currentDate=fromDate
+    datesList = []
+    while currentDate <= toDate:
+        datesList.append(currentDate)
+        currentDate += timedelta(7)
+    
+    datesList.append(toDate)
+        
+    return datesList
+        
+datesList = CreateDatesList(date(2020,6,2), date(2020,7,3))
+fromDateList = datesList[0:-1]
+toDateList = datesList[1:]
+for i,j in zip(fromDateList, toDateList):
+    print(i,j)
 # -
 
-SetUpEnvironmentVariables()
-tester = GetTweetsDataFrame('USATODAY',100)
 
-tester = tester.sort_values('id_str')
-print(tester.iloc[0])
-print(tester.iloc[-1])
+
+
+
+
+
+
+
+
+
+
+
+
+
+# ## old code
+
+
 
 # +
-# 1000 tweets - 5/6 till 18/6 = 2 weeks
-# 52 weeks = 1 year tweets = 26,000 tweets
-# 5 years tweets = 130,000 tweets
+
+# import sys
+# import os
+# import tweepy
+# import pandas as p
+
+# username = 'USATODAY'  # this is what we're searching for
+# maxTweets = 10 # Some arbitrary large number
+# tweetsPerQry = 2  # this is the max the API permits
+# fName = username+'_tweets.csv' # We'll store the tweets in a text file.
+
+
+# # If results from a specific ID onwards are reqd, set since_id to that ID.
+# # else default to no lower limit, go as far back as API allows
+# sinceId = None
+
+# # If results only below a specific ID are, set max_id to that ID.
+# # else default to no upper limit, start from the most recent tweet matching the search query.
+# max_id = -1
+
+# tweetCount = 0
+# print("Downloading max {0} tweets".format(maxTweets))
+
+# api = SetUpTwitterAPI()
+
+# tweetsDF = pd.DataFrame(
+#     columns=["id_str", "created_at", "full_text", "retweet_count"]
+# )
+# tweetsDF.to_csv('tweets.csv', mode='a')
+
+
+# while tweetCount < maxTweets:
+#     try:
+#         if (max_id <= 0):
+#             if (not sinceId):
+#                 new_tweets = tweepy.Cursor(
+#                     api.user_timeline, screen_name=username, 
+#                     tweet_mode="extended"
+#                 ).items(tweetsPerQry)
+#             else:
+#                 new_tweets = tweepy.Cursor(
+#                     api.user_timeline, screen_name=username, 
+#                     tweet_mode="extended", since_id=sinceId
+#                 ).items(tweetsPerQry)
+#         else:
+#             if (not sinceId):
+#                 new_tweets = tweepy.Cursor(
+#                     api.user_timeline, screen_name=username, 
+#                     tweet_mode="extended", max_id=str(max_id - 1)
+#                 ).items(tweetsPerQry)
+#             else:
+#                 new_tweets = tweepy.Cursor(
+#                     api.user_timeline, screen_name=username, 
+#                     tweet_mode="extended", max_id=str(max_id - 1),
+#                     since_id=sinceId
+#                 ).items(tweetsPerQry)
+#         if not new_tweets:
+#             print("No more tweets found")
+#             break
+#         tweetsDF = pd.DataFrame(
+#             columns=["id_str", "created_at", "full_text", "retweet_count"]
+#         )
+#         for tweet in new_tweets:
+#             tweetsDF = tweetsDF.append(CreateTweetDict(tweet), ignore_index=True)
+#         tweetsDF.to_csv('tweets.csv', mode='a', header=False)
+
+#         tweetCount += new_tweets.num_tweets
+#         print("Downloaded {0} tweets".format(tweetCount))
+#         max_id = new_tweets.current_page.max_id
+#     except tweepy.TweepError as e:
+#         # Just exit if any error
+#         print("some error : " + str(e))
+#         break
+
+# print ("Downloaded {0} tweets, Saved to {1}".format(tweetCount, fName))
+
+# -
+
+
--- notebooks/HelperFunctions.py
+++ notebooks/HelperFunctions.py
@@ -2,9 +2,19 @@ import configparser
 import os
 import tweepy
 import pandas as pd
+import GetOldTweets3 as got
+from datetime import datetime, timedelta, date
+import logging
 
 config = configparser.ConfigParser()
 config.read("code.conf")
+
+logging.basicConfig(
+    filename="tweet-download.log",
+    filemode="a",
+    format="%(asctime)s - %(message)s",
+    level=logging.INFO,
+)
 
 
 def SetUpEnvironmentVariables():
@@ -26,51 +36,129 @@ def SetUpEnvironmentVariables():
             os.environ[parameter] = config["DATA"][parameter]
 
 
-def GetTweetsDataFrame(username, lastNTweets):
+def CreateTweetDict(tweetInput, whichAPI):
     """
-    gets last N tweets of a particular user - returns DF containing the tweets
-    return: dataframe of tweets
+    Given a tweet object, take the relevant items and create custom dict
     """
-    auth = tweepy.AppAuthHandler(
-        os.environ["TWITTER_API_KEY"], os.environ["TWITTER_SECRET_KEY"]
-    )
-    api = tweepy.API(auth, wait_on_rate_limit=True, wait_on_rate_limit_notify=True)
+    tweetDict = {}
 
-    # get tweets
-    tweetsDF = pd.DataFrame(
-        columns=["id_str", "created_at", "full_text", "retweet_count"]
+    if whichAPI == "twitter":
+        tweetDict["id_str"] = int(tweetInput.id_str)
+        tweetDict["created_at"] = tweetInput.created_at
+        tweetDict["full_text"] = tweetInput.full_text.replace("\n", " ")
+        tweetDict["retweet_count"] = int(tweetInput.retweet_count)
+    elif whichAPI == "got3":
+        tweetDict["id"] = tweetInput.id
+        tweetDict["permalink"] = tweetInput.permalink
+        tweetDict["username"] = tweetInput.username
+        tweetDict["to"] = tweetInput.to
+        tweetDict["text"] = tweetInput.text
+        tweetDict["date"] = tweetInput.date
+        tweetDict["retweets"] = tweetInput.retweets
+        tweetDict["favorites"] = tweetInput.favorites
+        tweetDict["mentions"] = tweetInput.mentions
+        tweetDict["hashtags"] = tweetInput.hashtags
+        tweetDict["geo"] = tweetInput.geo
+
+    return tweetDict
+
+
+def GetOldTweetsIntoDF(username, fromDate, toDate=datetime.utcnow()):
+    """
+    Making use of library get-old-tweets-3 to download tweets into df
+    """
+    fromDateStr = fromDate.strftime("%Y-%m-%d")
+    toDateStr = toDate.strftime("%Y-%m-%d")
+    # Creation of query object
+    tweetCriteria = (
+        got.manager.TweetCriteria()
+        .setUsername(username)
+        .setSince(fromDateStr)
+        .setUntil(toDateStr)
     )
-    for tweet in tweepy.Cursor(
-        api.user_timeline, screen_name=username, tweet_mode="extended"
-    ).items(lastNTweets):
+    # Creation of list that contains all tweets
+    tweetsObject = got.manager.TweetManager.getTweets(tweetCriteria)
+    # Creating DF of chosen tweet data
+    tweetsDF = CreateEmptyTweetDataframe("got3")
+    for tweetItem in tweetsObject:
         try:
-            tweetsDF = tweetsDF.append(CreateTweetDict(tweet), ignore_index=True)
+            tweetsDF = tweetsDF.append(
+                CreateTweetDict(tweetItem, "got3"), ignore_index=True
+            )
         except:
             continue
 
     return tweetsDF
 
 
-def CreateTweetDict(tweetInput):
+def CreateEmptyTweetDataframe(whichAPI):
     """
-    Given a tweet object, take the relevant items and create custom dict
+    Creates an empty dataframe with correct headings
     """
-    tweetDict = {}
-    tweetDict["id_str"] = int(tweetInput.id_str)
-    tweetDict["created_at"] = tweetInput.created_at
-    tweetDict["full_text"] = tweetInput.full_text
-    tweetDict["retweet_count"] = int(tweetInput.retweet_count)
+    if whichAPI == "got3":
+        columnsList = [
+            "id",
+            "permalink",
+            "username",
+            "to",
+            "text",
+            "date",
+            "retweets",
+            "favorites",
+            "mentions",
+            "hashtags",
+            "geo",
+        ]
+    tweetsDF = pd.DataFrame(columns=columnsList)
 
-    return tweetDict
+    return tweetsDF
 
 
-def DownloadManyTweets(username, numberOfTweets):
+def SaveLoadOfOldTweetsIntoCSV(username, fromDate, toDate, fileName):
     """
-    Method to get LOADS of tweets
-    This will include a timer to not go over the twitter API limit
-    This will also keep track of tweet IDs so tweets are not duplicated
-    This will save results as a pkl file.
-    https://bhaskarvk.github.io/2015/01/how-to-use-twitters-search-rest-api-most-effectively./#:~:text=To%20start%20with%20the%20API,window%20for%20per%2Duser%20authentication.
+    Saves tweets to csv file on a weekly basis between two dates
     """
+
+    logging.info(
+        "Creating CSV file of tweets between {} and {}.".format(fromDate, toDate)
+    )
+
+    tweetsDF = CreateEmptyTweetDataframe("got3")
+    tweetsDF.to_csv(fileName)
+    datesList = CreateDatesList(fromDate, toDate)
+    fromDateList = datesList[0:-1]
+    toDateList = datesList[1:]
+
+    numberOfTweets = 0
+    for i, j in zip(fromDateList, toDateList):
+        currentTweetsDF = GetOldTweetsIntoDF(username, i, j)
+        numberOfTweets += len(currentTweetsDF)
+        currentTweetsDF.to_csv(fileName, mode="a", header=False)
+        logging.info(
+            "Update CSV file. Total number of Tweets: {} from {} till {}".format(
+                numberOfTweets, fromDate, j
+            )
+        )
+
+    logging.info(
+        "Completed the download of all {} tweets from {} into file: {} from {} till {}.".format(
+            numberOfTweets, username, fileName, fromDate, toDate
+        )
+    )
 
     return None
+
+
+def CreateDatesList(fromDate, toDate):
+    """
+    Create a vector of dates spaced by a week between the two input dates
+    """
+    currentDate = fromDate
+    datesList = []
+    while currentDate <= toDate:
+        datesList.append(currentDate)
+        currentDate += timedelta(7)
+
+    datesList.append(toDate)
+
+    return datesList
